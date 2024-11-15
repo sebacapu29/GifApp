c16e2e3ca966387e7840a608b2ec7ee20bbbbafb
Sebastian Capurro <sebacapu3@gmail.com>
2024-11-15 17:42:30 -0300

    creo nuevo componente addCategory y doy logica

--- gif-app/src/GifApp.jsx
+++ gif-app/src/GifApp.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react"
+import { AddCategory } from './components/AddCategory'
 
 export const GifApp = () =>{
     
     const [categories, setCategories] = useState(['Anime', 'Accion','Drama'])
-
+    
+    const onAddCategory = () => {
+        setCategories([...categories]);
+    }
     return(
         <>
         <h1>GifApp</h1>
+        <AddCategory setCategories={ setCategories }/>
         <ol>
-            {categories.map(category => {
+            { categories.map(category => {
                 return <li key={ category }> { category }</li>
             })}
         </ol>
---
+++ gif-app/src/components/AddCategory.jsx
@@ -0,0 +1,28 @@
+
+import { useState } from "react"
+
+export const AddCategory = ({setCategories}) => {
+    const [inputValue, setInputValue] = useState('One Punch')
+
+    const onInputChange = (event)=>{
+        setInputValue(event.target.value)
+    }
+    const onSubmit = (event) =>{
+        event.preventDefault();
+        if(inputValue.trim().length <=1) return;
+
+        setCategories((categories)=> [inputValue, ...categories]);
+        setInputValue('');
+    }
+    return (
+        <form onSubmit={(event)=> onSubmit(event)}>
+
+        <input 
+            type="text"
+            placeholder="Buscar gif"
+            value = { inputValue }
+            onChange={(event)=> onInputChange(event)}
+            />
+        </form>
+  )
+}
