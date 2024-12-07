d3cf018b32610093fd3fac1a6c83db668db2235f
Sebastian Capurro <sebacapu3@gmail.com>
2024-12-06 23:22:35 -0300

    completo getGifs

--- gif-app/src/services/getGifs.js
+++ gif-app/src/services/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category)=>{
-    const url = `/api/v1/gifs/search?api_key=sRP3Sc8MjSakzE2bUv1Lb0oVypSlCrod&q=${category}&limit=5`
+    const url = `/api/v1/gifs/search?api_key=x&q=${category}&limit=5`
     const resp = await fetch(url);
 
     const { data } = await resp.json();
